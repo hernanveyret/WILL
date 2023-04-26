3ff216ea0f774b312d24c12e1101ee67eae9ebd6
hernanVeyret <hernanveyret@hotmail.com>
2023-04-26 19:30:35 -0300

    01 Finalizado

--- 01.js
+++ 01.js
@@ -11,6 +11,8 @@ function soloNumeros(array) {
   // soloNumeros([1, 'Henry', 2]) debe retornar [1, 2]
 
   // Tu código aca:
+  let numeros = array.filter((e) => Number.isInteger(e))
+  return numeros;
 
 }
 
