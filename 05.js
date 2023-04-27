d5c0a4e3aba66a09428ffc9784c8224aacfff454
hernanVeyret <hernanveyret@hotmail.com>
2023-04-26 21:32:57 -0300

    05 Finalizado

--- 05.js
+++ 05.js
@@ -12,9 +12,10 @@ function pluck(array, propiedad) {
   // var productos = [{ name: 'TV LCD', price: 100}, { name: 'Computadora', price: 500 }]
   // productos.pluck(productos, 'name') debería devolver ['TV LCD', 'Computadora']
   // Pista: es una buena oportunidad para usar map.
-
   // Tu código acá:
-
+  
+const nuevoArr = array.map((e) => e[propiedad]);
+return nuevoArr
 }
 
 // No modifiques nada debajo de esta linea //
