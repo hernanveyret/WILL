60f0e4a41834f0b9e034fe5d4a2a9809fcba7cb1
hernanVeyret <hernanveyret@hotmail.com>
2023-04-26 21:30:35 -0300

    03 Finalizado

--- 03.js
+++ 03.js
@@ -12,8 +12,13 @@ function buscarAmigo(amigos, nombre) {
   // Ej:
   //  var amigos = [{ nombre: 'toni', edad: 33 } , { nombre: 'Emi', edad: 25 }];
   //  buscarAmigo(amigos, 'toni') debe devolver { nombre: 'toni', edad: 33 };
-
   // Tu código aca:
+
+  for ( i = 0; i < amigos.length; i++) {
+    if ( amigos[i].nombre === nombre) {
+      return amigos[i]
+    }
+  }
   
 }
 
